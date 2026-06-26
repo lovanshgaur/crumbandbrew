1e9aa7557896c03e3722dadebd836337a304eb9f
lovanshgaur <lovanshgaur@gmail.com>
2026-06-27 04:09:59 +0530

    homepage space reserved

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1>crumb&brew_v2</h1>
+    <iframe
+      src="/index.html"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        display: "block",
+      }}
+    />
   );
 }
